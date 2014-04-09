85d70a725895ea7f1b50d033c21f8634daa52baa
Lukas Erlacher <erlacher@in.tum.de>
2014-04-09 20:29:59 +0200

    Initial Commit

---
+++ forward.sh
@@ -0,0 +1,8 @@
+WAN=enp0s29f7u1i8
+LAN=enp1s0
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE
+iptables -A FORWARD -i $WAN -o $LAN -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i $LAN -o $WAN -j ACCEPT
